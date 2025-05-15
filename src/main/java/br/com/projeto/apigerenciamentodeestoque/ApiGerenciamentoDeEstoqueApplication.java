04778e421bdd2da24a0c588e6a8225066d934e15
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-05-14 21:36:51 -0300

    :sparkles: feat: Criação de Endpoint Endepoint: /user BackLog: #6

---
+++ src/main/java/br/com/projeto/apigerenciamentodeestoque/ApiGerenciamentoDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.apigerenciamentodeestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciamentoDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGerenciamentoDeEstoqueApplication.class, args);
+	}
+
+}
