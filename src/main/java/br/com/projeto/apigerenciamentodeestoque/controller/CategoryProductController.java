f22515bbd74c64e69f7937e851a756488077d4c5
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-05-26 11:19:33 -0300

    :book: docs: atualização da documentação

--- src/main/java/br/com/projeto/apigerenciamentodeestoque/controller/CategoryProductController.java
+++ src/main/java/br/com/projeto/apigerenciamentodeestoque/controller/CategoryProductController.java
@@ -4,6 +4,9 @@ import br.com.projeto.apigerenciamentodeestoque.DTOs.RegisterCategoryProductDto;
 import br.com.projeto.apigerenciamentodeestoque.DTOs.UpdateCategoryProductDto;
 import br.com.projeto.apigerenciamentodeestoque.model.Product.CategoryProduct;
 import br.com.projeto.apigerenciamentodeestoque.service.CategoryProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,31 +20,56 @@ public class CategoryProductController {
 
     @Autowired
     CategoryProductService categoryProductService;
-    
+
+    @Operation(summary = "Cadastrar nova categoria de produto", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Categoria cadastrada com sucesso"),
+            @ApiResponse(responseCode = "409", description = "Categoria já existe")
+    })
     @PostMapping("/create")
     public ResponseEntity createCategoryProduct(@RequestBody RegisterCategoryProductDto registerCategoryProductDto) {
         CategoryProduct categoryProduct = categoryProductService.createCategoryProduct(registerCategoryProductDto);
         return ResponseEntity.status(201).body(categoryProduct);
     }
 
+    @Operation(summary = "Listar categorias de produto", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categorias listadas com sucesso")
+    })
     @GetMapping("/list")
     public ResponseEntity<List<CategoryProduct>> listCategoryProducts(@RequestParam(name = "name", required = false) String name) {
         List<CategoryProduct> categoryProducts = categoryProductService.listCategoryProducts(name);
         return ResponseEntity.ok().body(categoryProducts);
     }
 
+    @Operation(summary = "Buscar categoria por nome", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria encontrada"),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada")
+    })
     @GetMapping
     public ResponseEntity<CategoryProduct> getCategoryByName(@RequestParam(name = "name") String name) {
         var categoryProducts = categoryProductService.categoryByName(name);
         return ResponseEntity.ok().body(categoryProducts);
     }
 
+    @Operation(summary = "Atualizar categoria de produto", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Categoria atualizada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Campos obrigatórios vazios"),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada")
+    })
     @PutMapping("/update")
     public ResponseEntity<CategoryProduct> updateCategoryProduct(@RequestParam(name="name") String name, @RequestBody UpdateCategoryProductDto updateCategoryProductDto) {
         CategoryProduct updatedCategoryProduct = categoryProductService.updateCategoryProduct(name, updateCategoryProductDto);
         return ResponseEntity.ok().body(updatedCategoryProduct);
     }
 
+    @Operation(summary = "Deletar categoria de produto", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Categoria deletada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada")
+    })
     @DeleteMapping("/delete/{name}")
     public ResponseEntity deleteCategoryProduct(@PathVariable String name) {
         categoryProductService.deleteCategoryProduct(name);
--- src/main/java/br/com/projeto/apigerenciamentodeestoque/controller/ProductController.java
+++ src/main/java/br/com/projeto/apigerenciamentodeestoque/controller/ProductController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping("/produto")
 public class ProductController {
@@ -18,24 +22,46 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Operation(summary = "Cadastro de produto", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Produto cadastrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Categoria do produto obrigatória"),
+            @ApiResponse(responseCode = "409", description = "Produto já existe")
+    })
     @PostMapping("/register")
     public ResponseEntity<?> createProduct(@RequestBody ProductDTO productDTO) {
         Product createdProduct = productService.createProduct(productDTO);
         return ResponseEntity.status(201).body(createdProduct);
     }
 
+    @Operation(summary = "Listar produtos", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produtos listados com sucesso")
+    })
     @GetMapping("/listar")
     public ResponseEntity<?> listarProdutos(@RequestParam(required = false, name = "name") String name) {
         List<Product> produtos = productService.listarProdutos(name);
         return ResponseEntity.ok().body(produtos);
     }
 
+    @Operation(summary = "Atualizar produto", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado"),
+            @ApiResponse(responseCode = "404", description = "Categoria não encontrada")
+    })
     @PutMapping("/update")
     public ResponseEntity<Product> atualizar(@RequestParam(name = "name") String name, @RequestBody UpdateProductDTO dto) {
         Product product = productService.atualizarProduto(name,dto);
         return ResponseEntity.ok().body(product);
     }
 
+    @Operation(summary = "Desativar produto", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Produto desativado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Produto não encontrado"),
+            @ApiResponse(responseCode = "409", description = "Produto já está desativado")
+    })
     @PutMapping("/desativar")
     public ResponseEntity<Product> desativarProduto(@RequestParam String name) {
         Product produto = productService.desativarProduto(name);
